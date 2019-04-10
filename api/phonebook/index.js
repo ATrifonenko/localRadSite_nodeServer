fe8e76855c0e734206e58da146dc069df6cd8807
Andrey Trifonenko <sintez91@gmail.com>
2019-04-10 21:56:48 +0500

    addRow basic

--- api/phonebook/index.js
+++ api/phonebook/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const getPhoneBook = require("./getPhoneBook");
+const addRow = require("./addRow");
 
 const phonebook = express.Router();
 
 phonebook.get("/getPhoneBook", getPhoneBook);
+phonebook.get("/addRow", addRow);
 
 module.exports = phonebook;
